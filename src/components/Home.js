34f3d87c10798877b9bf6193f1e3c99e477f4d6d
michal-hynek <hynek.michal@gmail.com>
2022-03-20 14:54:11 -0700

    Added (un)answered questions data to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
 import NavBar from './NavBar';
 
-const Home = () => {
+const mapStateToProps = (state) => {
+    const answeredQuestions = Object.keys(state.questions)
+        .filter(questionId => {
+            return Object.keys(state.users[state.currentUser].answers).includes(questionId);
+        })
+        .map(questionId => (state.questions[questionId]))
+        .sort((a, b) => b.timestamp - a.timestamp);
+
+    const unansweredQuestions = Object.keys(state.questions)
+        .filter(questionId => {
+            return !Object.keys(state.users[state.currentUser].answers).includes(questionId);
+        })
+        .map(questionId => state.questions[questionId])
+        .sort((a, b) => b.timestamp - a.timestamp);
+
+    return {
+        answeredQuestions,
+        unansweredQuestions,
+    };
+};
+
+const Home = (props) => {
     return (
         <div>
             <NavBar />
@@ -10,4 +32,4 @@ const Home = () => {
     );
 };
 
-export default Home;
+export default connect(mapStateToProps)(Home);
